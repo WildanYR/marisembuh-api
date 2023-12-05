69a551e8c7d5cf3e4395ca557f27b20bdbf8f1b6
WildanYR <wildanyahya99@gmail.com>
2023-12-06 06:01:08 +0700

    add: exception filter middleware

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ApiExceptionFilter } from './middlewares/exception_filter.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const httpAdapterHost = app.get(HttpAdapterHost);
   const configService = app.get(ConfigService);
 
   app.setGlobalPrefix(configService.get('app.api_prefix'));
+  app.useGlobalFilters(new ApiExceptionFilter(httpAdapterHost));
 
   await app.listen(configService.get('app.port'));
 }
