974cbe64e2eeadb68b4a8ad4fb355b3d5b622b87
WildanYR <wildanyahya99@gmail.com>
2023-12-06 06:10:33 +0700

    add: global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ApiExceptionFilter } from './middlewares/exception_filter.middleware';
+import { ValidationError, ValidationPipe } from '@nestjs/common';
+import { InvalidDataException } from './exceptions/invalid_data.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -9,6 +11,13 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.setGlobalPrefix(configService.get('app.api_prefix'));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (errors: ValidationError[]) =>
+        new InvalidDataException(errors),
+    }),
+  );
   app.useGlobalFilters(new ApiExceptionFilter(httpAdapterHost));
 
   await app.listen(configService.get('app.port'));
