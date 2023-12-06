4cfee01396d4df6f8d49dc351e8fc32c1a091063
WildanYR <wildanyahya99@gmail.com>
2023-12-06 11:42:11 +0700

    add: sequelize configuration

---
+++ .sequelizerc.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  dialect: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DB,
+};
